1f507eac55a6882563f727e97f49df2f2d10ab44
almondragon <158229402+almondragon@users.noreply.github.com>
2024-10-01 15:15:44 -0600

    modified SmartStrategy.php for pick_place() function

--- OmokService/src/play/SmartStrategy.php
+++ OmokService/src/play/SmartStrategy.php
@@ -5,33 +5,46 @@ require_once "MoveStrategy.php";
     
 class SmartStrategy extends MoveStrategy{
     
-    function pickPlace(){
-        //intialize the board
+    function pick_place(){
+        
+        // checks if can win
+        
         $board = $this->board;
-        $place = $this->checkThreeWithOpenEndOpponent(2);
-        return $place;
+        
+        $win_place = $this->checkThreeWithOpenEndOpponent(1);
+        if($win_place !== null){
+            return $win_place;
+        }
+        
+        // checks if block is neccessary
+        $block_place = $this->checkThreeWithOpenEndOpponent(2);
+        if($block_place !== null){
+            return $block_place;
+        }
+        
+        // if neither, random spot is chosen
+        
+        $emptySpots = $board->find_empty_spots();
+        return $emptySpots[array_rand($emptySpots)];
 
         
-        //see if win
-        //see if block
-        //return random otherwise
-        
+
     }
     
     function checkThreeWithOpenEndOpponent($player){
         $board = $this->board;
-        $size = $board->getSize();
+        $size = $board->get_size();
         //check vertical row
         for($x=0;$x<$size;$x++){
             for($y=0;$y<$size - 2;$y++){
-                if($board->arrayBoard[$x][$y]== $player && $board->arrayBoard[$x][$y + 1]== $player && $board->arrayBoard[$x][$y + 2]== $player){
+                if($board->array_board[$x][$y]== $player && $board->array_board[$x][$y + 1]== $player && $board->array_board[$x][$y + 2]== $player){
                     // check that the next two spaces are filled
                     // check if the open end is on the top of the row
-                    if($y>0 && $board->arrayBoard[$x][$y-1] == 0){
+                    if($y>0 && $board->array_board[$x][$y-1] == 0){
                         return [$x,$y-1];
                     }
                     // check if the open end is on the bottom of the row
-                    if($y+3<$size && $board->arrayBoard[$x][$y+3] == 0)
+                    if($y+3<$size && $board->array_board[$x][$y+3] == 0)
                         return [$x,$y+3];
                 }
             }
@@ -39,13 +52,13 @@ class SmartStrategy extends MoveStrategy{
         //check horizontal row
         for($x=0;$x<$size-2;$x++){
             for($y=0;$y<$size;$y++){
-                if($board->arrayBoard[$x][$y]== $player && $board->arrayBoard[$x+1][$y]== $player && $board->arrayBoard[$x+2][$y]== $player){
+                if($board->array_board[$x][$y]== $player && $board->array_board[$x+1][$y]== $player && $board->array_board[$x+2][$y]== $player){
                     // checks if open end is on left side
-                    if($x>0 && $board->arrayBoard[$x-1][$y] == 0){
+                    if($x>0 && $board->array_board[$x-1][$y] == 0){
                         return [$x-1,$y];
                     }
                     // checks if open end is on right side
-                    if($x+3<$size && $board->arrayBoard[$x+3][$y] == 0)
+                    if($x+3<$size && $board->array_board[$x+3][$y] == 0)
                         return [$x+3,$y];
                 }
             }
@@ -54,13 +67,13 @@ class SmartStrategy extends MoveStrategy{
         //check diagonal row (bottom-right)
         for($x=0;$x<$size-2;$x++){
             for($y=0;$y<$size-2;$y++){
-                if($board->arrayBoard[$x][$y]==$player && $board->arrayBoard[$x+1][$y+1]== $player && $board->arrayBoard[$x+2][$y+2] == $player){
+                if($board->array_board[$x][$y]==$player && $board->array_board[$x+1][$y+1]== $player && $board->array_board[$x+2][$y+2] == $player){
                     //check if open end is on top-left corner of the diagonal row
-                    if($x>0 && $board->arrayBoard[$x-1][$y-1]==0){
+                    if($x>0 && $board->array_board[$x-1][$y-1]==0){
                         return [$x-1,$y-1];
                     }
                     //check if open end is on bottom-right corner of the diagonal row
-                    if($x+3>0 && $board->arrayBoard[$x+3][$y+3]==0){
+                    if($x+3>0 && $board->array_board[$x+3][$y+3]==0){
                         return [$x+3,$y+3];
                     }
                 }
@@ -71,19 +84,19 @@ class SmartStrategy extends MoveStrategy{
     
         for($x=2; $x < $size; $x++){
             for($y=0;$y < $size - 2; $y++){
-                if($board->arrayBoard[$x][$y] == $player && $board->arrayBoard[$x-1][$y+1] == $player && $board->arrayBoard[$x-2][$y+2]==$player){
+                if($board->array_board[$x][$y] == $player && $board->array_board[$x-1][$y+1] == $player && $board->array_board[$x-2][$y+2]==$player){
                     //check if open end is on top-right corner of diagonal row
-                    if($x - 3 >= 0 && $y+3 < $size && $board->arrayBoard[$x-3][$y+3]==0){
+                    if($x - 3 >= 0 && $y+3 < $size && $board->array_board[$x-3][$y+3]==0){
                         return [$x-3,$y+3];
                     }
                     //check if open end is on bottom-left corner of the diagonal row
-                    if($x + 1 < $size && $y > 0 && $board->arrayBoard[$x+1][$y-1]){
+                    if($x + 1 < $size && $y > 0 && $board->array_board[$x+1][$y-1]){
                         return [$x+1,$y-1];
                     }
                 }
             }
         }
-        
+        return null;
         
     }
     
