fe2d87b358452eded4ca114cb3d90be41b594d70
christian dees <christianmdees@mgail.com>
2024-10-01 20:21:42 -0600

    minor syntax error

--- OmokService/src/play/SmartStrategy.php
+++ OmokService/src/play/SmartStrategy.php
@@ -148,10 +148,10 @@ class SmartStrategy extends MoveStrategy{
         [$x, $y] = explode(',', $max_key);
         // directions for new spot
         $directions = [
-            [-1, 0], // up
-            [1, 0],  // down
-            [0, -1], // left
-            [0, 1],  // right
+            [-1, 0],  // up
+            [1, 0],   // down
+            [0, -1],  // left
+            [0, 1],   // right
             [-1, -1], // up-left
             [-1, 1],  // up-right
             [1, -1],  // down-left
